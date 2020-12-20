1516851d9ead8456ba3bb4a2a41f10e687d2bbd1
thinhphan97 <thinhphan130997@gmail.com>
2020-12-20 08:47:52 +0700

    add Department

---
+++ human_resource_management/businessobject/Department.h
@@ -0,0 +1,17 @@
+#ifndef  _businessobject_Department_h_
+#define  _businessobject_Department_h_
+#include <string>
+
+using namespace std;
+
+class Department{
+    string Dname;
+    int Dnumber;
+    int MgrSSN;
+    string Mgrstartdate;
+public:
+    Department();
+    Department(string dname, int dnumber, int mgrSSN, string mgrstartdate);
+    void Print_department();
+};
+#endif
