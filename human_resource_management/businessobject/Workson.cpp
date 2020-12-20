fa186d5fb397e9dadfcffb3b192d7bf73ec4c0fd
thinhphan97 <thinhphan130997@gmail.com>
2020-12-20 21:49:01 +0700

    add file Works_on

---
+++ human_resource_management/businessobject/Workson.cpp
@@ -0,0 +1,8 @@
+#include "Workson.h"
+
+Workson::Workson(){};
+Workson::Workson(int essn, int pno, int hours){
+    ESSN =essn;
+    PNO = pno;
+    Hours = hours;
+}
---
+++ human_resource_management/businessobject/Workson.h
@@ -0,0 +1,12 @@
+#ifndef _businessobject_Workson_h_
+#define _businessobject_Workson_h_
+
+class Workson
+{
+    int ESSN, PNO;
+    float Hours; 
+public:
+    Workson();
+    Workson(int essn, int pno, int hours);
+};
+#endif
